538bf94950599e14104eef30cd54ded53b62f29e
Karol Szymański <karol2134@op.pl>
2023-12-18 16:38:28 +0100

    Importing the reactor component to check how it will look in electron

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 
-
+import Reactor from './components/reactor/reactor';
+import styled from 'styled-components';
 
 
 
@@ -11,13 +11,26 @@ function App() {
   };
 
   return (
+    <MainContainer>
     <div className="App">
       <header className="App-header">
         <h1>Electron React Paint</h1>
         <button onClick={openPaint}>Open Paintttt</button>
+        <Reactor></Reactor>
       </header>
     </div>
+    </MainContainer>
   );
 }
 
 export default App;
+
+
+
+const MainContainer = styled.div`
+  background-color: black; 
+  width: 100%;
+  height: 100vh;
+`
+
+
